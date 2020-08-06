8f3a89ea35860100d517763825b4c4c83cef76f4
allanrehhoff <allan.rehhoff@gmail.com>
2020-08-06 22:20:09 +0200

    Adding random wiki quote command

--- commands/index.js
+++ commands/index.js
@@ -1,7 +1,8 @@
 module.exports = {
 	commands: {
 		role: require("./src/role.js"),
-		character: require("./src/character.js")
+		character: require("./src/character.js"),
+		wiki: require("./src/wiki.js")
 	},
 	exists: function(cmd) {
 		return typeof this.commands[cmd] != "undefined";
