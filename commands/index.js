0683b0a4e51fc17f91476fd7bd3d778f4973ad4f
Chris <christopher.gilpatrick@gmail.com>
2020-08-06 20:06:58 +0100

    require design js

--- commands/index.js
+++ commands/index.js
@@ -1,7 +1,8 @@
 module.exports = {
 	commands: {
 		role: require("./src/role.js"),
-		character: require("./src/character.js")
+		character: require("./src/character.js"),
+		design: require("./src/design.js")
 	},
 	exists: function(cmd) {
 		return typeof this.commands[cmd] != "undefined";
