981fcf299bec2282d0212668a44a111e70a5f9a5
Allan Rehhoff <allan@sharksmedia.dk>
2020-08-07 11:58:57 +0200

    Adding help command

--- commands/index.js
+++ commands/index.js
@@ -2,7 +2,8 @@ module.exports = {
 	commands: {
 		role: require("./src/role.js"),
 		character: require("./src/character.js"),
-		wiki: require("./src/wiki.js")
+		wiki: require("./src/wiki.js"),
+		help: require("./src/help.js")
 	},
 	exists: function(cmd) {
 		return typeof this.commands[cmd] != "undefined";
---
+++ commands/src/help.js
@@ -0,0 +1,59 @@
+module.exports = {
+	help: {
+
+wiki: `
+wiki
+	Available subcommands: quote
+
+	Fetches a random quote from the legacy wiki, for funs and giggles.
+
+	@mention wiki quote
+`,
+
+role: `
+role
+	Available subcommands: Shadress, Ritual, Slums
+
+	Add/remove roles using this format in any channel.
+
+	@mention role Shadress
+	@mention role Ritual
+	@mention role Slums
+`,
+
+character: `
+character
+	Available subcommands: <player>
+
+	Displays a character card from any given player.
+
+	@mention role Lynx
+`
+	},
+	getHelp: function(cmd) {
+		if(typeof this.help[cmd] !== "undefined") {
+			let helpmsg = "```" + this.help[cmd] + "```";
+			return helpmsg;
+		}
+
+		return '';
+	},
+	getAllHelp: function() {
+		let result = "";
+
+		Object.keys(this.help).forEach(cmd => {
+			result += ' ' + this.getHelp(cmd);
+		});
+
+		return result;
+	},
+	execute: function(msg, args) {
+		if(typeof args[2] != "undefined") {
+			var help = this.getHelp(args[2]);
+		} else {
+			var help = this.getAllHelp(msg);
+		}
+
+		msg.channel.send("BRRRR.. Providing Help... PROCESS COMPLETED\n" + help);
+	}
+}
